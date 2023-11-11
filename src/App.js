2978c0dc9f8fa53e4ace58a021270acee0f14532
Nandini <snandiniuday@gmail.com>
2023-11-11 18:01:12 +0530

    certifications

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import About from './Components/About';
 
 //css files
 import './App.css';
+import Certifications from './Components/Certifications';
 
 function App() {
   return (
     <div className="App">
       <NavbarComponents />
       <About />
+      <Certifications />
     </div>
   );
 }
---
+++ src/Components/Certifications.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import './Certifications.css';
+import CardComponent from './CardComponent';
+
+function Certifications() {
+    return (
+        <div className="cert-main">
+            <div className="cert-title">
+            <h5>Certifications</h5>
+            <p>Here are some of my certifications</p>
+            </div>
+            <CardComponent />
+        </div>
+    )
+}
+
+export default Certifications
